41693dfc9f79780f4bfe5082794f03510be84594
ShakhovSergey <shahov12345@yandex.ru>
2022-07-17 01:26:43 +0300

    controllers-films-users 1.2 feat добавить логгирование

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.FilmAlreadyExistException;
 import ru.yandex.practicum.filmorate.exception.FilmIsNotCorrectException;
@@ -12,6 +13,7 @@ import java.util.HashMap;
 
 @RestController
 @RequestMapping("/films")
+@Slf4j
 public class FilmController {
     private final static String EMPTY_STRING = "";
     private final static LocalDate CINEMA_BIRTHDATE = LocalDate.of(1895, 12, 28);
@@ -20,6 +22,7 @@ public class FilmController {
 
     @PostMapping
     public Film addFilm(@RequestBody Film film) {
+        log.info("Получен запрос к эндпоинту: POST /films, Создан объект из тела запроса:'{}'", film);
         validateFilm(film);
         if (films.containsValue(film)) {
             throw new FilmAlreadyExistException("This film is already exist");
@@ -31,21 +34,26 @@ public class FilmController {
 
     private void validateFilm(Film film) {
         if (EMPTY_STRING.equals(film.getName())) {
+            log.warn("Исключение. Name is empty. Объект из тела запроса:'{}'", film);
             throw new FilmIsNotCorrectException("Film is not correct. Name is empty.");
         }
         if (film.getDescription().length() > 200) {
+            log.warn("Исключение. Description is more than 200 chars. Объект из тела запроса:'{}'", film);
             throw new FilmIsNotCorrectException("Film is not correct. Description is more than 200 chars.");
         }
         if (film.getReleaseDate().isBefore(CINEMA_BIRTHDATE)) {
+            log.warn("Исключение. Release date is before than CINEMA_BIRTHDATE. Объект из тела запроса:'{}'", film);
             throw new FilmIsNotCorrectException("Film is not correct. Release date is before than " + CINEMA_BIRTHDATE);
         }
         if (film.getDuration() <= 0) {
+            log.warn("Исключение. Duration is not positive. Объект из тела запроса:'{}'", film);
             throw new FilmIsNotCorrectException("Film is not correct. Duration is not positive.");
         }
     }
 
     @PutMapping
     public Film updateFilm(@RequestBody Film film) {
+        log.info("Получен запрос к эндпоинту: PUT /films, Создан объект из тела запроса:'{}'", film);
         validateFilm(film);
         Integer id = film.getId();
         if (id == null) {
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.*;
 import ru.yandex.practicum.filmorate.model.User;
@@ -10,6 +11,7 @@ import java.util.HashMap;
 
 @RestController
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
     private final static String EMPTY_STRING = "";
     private static HashMap<Integer, User> users = new HashMap<>();
@@ -17,6 +19,7 @@ public class UserController {
 
     @PostMapping
     public User createUser(@RequestBody User user) {
+        log.info("Получен запрос к эндпоинту: POST /users, Создан объект из тела запроса:'{}'", user);
         validateUser(user);
         if (EMPTY_STRING.equals(user.getName())) {
             user.setName(user.getLogin());
@@ -32,19 +35,23 @@ public class UserController {
     private void validateUser(User user) {
         String email = user.getEmail();
         if (EMPTY_STRING.equals(email) || !email.contains("@")) {
+            log.warn("Исключение. Email is wrong. Объект из тела запроса:'{}'", user);
             throw new UserIsNotCorrectException("User is not correct. Email is wrong.");
         }
         String login = user.getLogin();
         if (EMPTY_STRING.equals(login) || login.contains(" ")) {
+            log.warn("Исключение. Login is wrong. Объект из тела запроса:'{}'", user);
             throw new UserIsNotCorrectException("User is not correct. Login is wrong.");
         }
         if (user.getBirthday().isAfter(LocalDate.now())) {
+            log.warn("Исключение. Birthday is wrong. Объект из тела запроса:'{}'", user);
             throw new UserIsNotCorrectException("User is not correct. Birthday is wrong");
         }
     }
 
     @PutMapping
     public User updateUser(@RequestBody User user) {
+        log.info("Получен запрос к эндпоинту: PUT /users, Создан объект из тела запроса:'{}'", user);
         validateUser(user);
         Integer id = user.getId();
         if (id == null) {
